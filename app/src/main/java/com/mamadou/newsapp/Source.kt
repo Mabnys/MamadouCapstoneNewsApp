41f0c4d45c283ae23f5e1d878a108f76af260593
Mamadou Balde <mamadou.balde@its.ny.gov>
2022-09-05 02:59:48 -0400

    Completed The Remaining Need To Have
    - Null checks are done using if/else statements
    - Code is divided into multiple functions...

--- app/src/main/java/com/mamadou/newsapp/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.mamadou.newsapp
-
-data class Source(
-    val id: String? = null,
-    val name: String
-)
---
+++ app/src/main/java/com/mamadou/newsapp/models/Article.kt
@@ -0,0 +1,11 @@
+package com.mamadou.newsapp.models
+
+data class Article(
+    val author: String? = null,
+    val title: String,
+    val description: String,
+    val url: String? = null,
+    val urlToImage: String,
+    val publishedAt: String,
+    val content: String? = null,
+)
